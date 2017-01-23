b1e04f46c1379f96bfd1391e3419074168879c71
Jan Szumiec <jan.szumiec@gmail.com>
2017-01-23 22:01:11 +0000

    Initial attempt at getting the GP2Y10 working.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <Arduino.h>
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Air Quality Sensor");
+  Serial.print("Built on ");
+  Serial.print(__DATE__);
+  Serial.print(__TIME__);
+  Serial.print(" commit ");
+  Serial.println(GIT_REVISION);
+
+  pinMode(D0, OUTPUT);
+  pinMode(A0, INPUT);
 }
 
 void loop() {
+  digitalWrite(D0, HIGH);
+  delayMicroseconds(280);
+  int a0 = analogRead(A0);
+  delayMicroseconds(40);
+  digitalWrite(D0, LOW);
+
+  Serial.println(a0);
+
+  delay(1000);
 }
