fdf7019bad0e2cc020a7338e32dbba57a894a071
Jan Szumiec <jan.szumiec@gmail.com>
2017-02-16 22:40:24 +0000

    Initial stab at implementing support for the SDS011.

--- src/sensors/sds011.cpp
+++ src/sensors/sds011.cpp
@@ -1,13 +1,40 @@
 #include "sds011.h"
 
-void SDS011::begin() {
-
+bool sds011_valid_packet(sds011_packet_t *pkt) {
+  if (pkt->header != '\xaa' || pkt->tail != '\xab') return false;
+  
+  unsigned char checksum = pkt->pm25lo + pkt->pm25hi + pkt->pm10lo + pkt->pm10hi + pkt->id1 + pkt->id2;
+  return checksum == pkt->checksum;
 }
 
-bool SDS011:is_operational() {
+void SDS011::begin() {
+  // TODO: Consider passing this in
+  Serial1.begin(9600);
+}
 
+bool SDS011::is_operational() {
+  sds011_packet_t packet;
+  memset(&packet, 0, sizeof(sds011_packet_t));
+
+  read_packet(&packet);
+  return sds011_valid_packet(&packet);
 }
 
 bool report(JsonObject &parent) {
+  return true;
+}
 
+bool read_packet(sds011_packet_t *pkt) {
+  int attempts = 20;
+
+  while (attempts-- > 0) {
+    unsigned char hdr = Serial1.read();
+    pkt->header = hdr;
+    if (hdr == '\xaa') {
+      Serial1.readBytes(&pkt->cmd, sizeof(sds011_packet_t) - 1);
+      if (sds011_valid_packet(pkt)) return true;
+    }
+  }
+
+  return false;
 }
--- src/sensors/sds011.h
+++ src/sensors/sds011.h
@@ -1,13 +1,31 @@
 #ifndef _SDS011_H
 #define _SDS011_H
 
+#include <HardwareSerial.h>
 #include <ArduinoJson.h>
 
+struct sds011_packet_t {
+  unsigned char header;
+  unsigned char cmd;
+  unsigned char pm25lo;
+  unsigned char pm25hi;
+  unsigned char pm10lo;
+  unsigned char pm10hi;
+  unsigned char id1;
+  unsigned char id2;
+  unsigned char checksum;
+  unsigned char tail;
+};
+
+bool sds011_valid_packet(sds011_packet_t *pkt);
+
 class SDS011 {
-public:
+ public:
   void begin();
   bool is_operational();
   bool report(JsonObject &);
+
+  bool read_packet(sds011_packet_t *);
 };
 
 #endif
