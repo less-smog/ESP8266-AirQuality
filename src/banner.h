79d325dcd555da78b2bfdb35ecea1b7e36fa0502
Jan Szumiec <jan.szumiec@gmail.com>
2017-08-07 21:34:09 +0100

    Attempt to fix compilation issue on Windows.

--- src/banner.h
+++ src/banner.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define BANNER "\n\nless-smog.org Air Quality Sensor" "\n" "Built on " __DATE__ " " __TIME__ " commit " GIT_REVISION
+#define BANNER ("\n\nless-smog.org Air Quality Sensor" "\n" "Built on " __DATE__ " " __TIME__ " commit " GIT_REVISION)
 
 void banner() {
     Serial.println(F(BANNER));
